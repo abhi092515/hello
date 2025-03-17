7a09cd4b453d488c062751b9823038617e8c2d99
Abhi Sihag <abhisihag@Abhis-MacBook-Air-3.local>
2025-03-17 16:41:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/youtube', (req, res) => {
+  res.send('login here for yt')
+})
+app.get('/twitter', (req, res) => {
+  res.send("<h2>hello twitter</h2>")
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
